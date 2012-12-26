12684b7fa1e3df02181493eaa9cee4e7fc9b7b38
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-27 00:24:47 +0100

    Release 1.0.0 with fixed dependencies.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-filesystem-service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.99.0"
+version          "1.0.0"
 
 %w( ubuntu ).each do |os|
   supports os
